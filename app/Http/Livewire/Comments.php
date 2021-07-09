8589eb693fb3a6aa70a386bdd4762713aac28157
akasifislam <aseasifislam@gmail.com>
2021-07-09 15:30:08 +0600

    laravel liveware wire click event

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -2,22 +2,36 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
-    public $comments = [
-        [
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloribus, placeat modi deleniti optio explicabo et aut nam ullam reiciendis porro, tenetur aperiam. Quibusdam cupiditate nam delectus totam doloribus, minima placeat quasi alias ullam numquam sequi ab! In, ex. Id, itaque! Officia nihil illo corporis, labore beatae quo ullam debitis nesciunt alias corrupti aperiam. Aperiam libero deserunt perspiciatis non suscipit.',
-            'created_at' => '3 min ago',
-            'creator' => 'Sarthak'
-        ]
-    ];
-
-
 
     public function render()
     {
         return view('livewire.comments');
+    }
+
+    public $comments = [
+        [
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloribus, placeat modi deleniti optio explicabo et aut nam ullam reiciendis porro, tenetur aperiam. Quibusdam cupiditate nam delectus totam doloribus, minima placeat quasi alias ullam numquam sequi ab! In, ex. Id, itaque! Officia nihil illo corporis, labore beatae quo ullam debitis nesciunt alias corrupti aperiam. Aperiam libero deserunt perspiciatis non suscipit.',
+            'created_at' => '3 min ago',
+            'creator' => 'Asif Ul Islam'
+        ]
+    ];
+
+    public $newComment;
+
+
+
+    public function addComment()
+    {
+        array_unshift($this->comments, [
+            'body' => $this->newComment,
+            'created_at' => Carbon::now()->diffForHumans(),
+            'creator' => 'masterman'
+        ]);
+        $this->newComment = "";
     }
 }
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -2,10 +2,10 @@
         <div class="w-6/12">
             <h1 class="my-10 text-3xl">comments</h1>
             <div class="my-4 flex">
-            <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="what's in your mind">
+            <input type="text" wire:model="newComment" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="what's in your mind">
 
             <div class="py-2">
-                <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">add</button>
+                <button wire:click="addComment" class="p-2 bg-blue-500 w-20 rounded shadow text-white">add</button>
             </div>
         </div>
         @foreach ($comments as $comment)
@@ -14,9 +14,7 @@
                     <p class="font-bold text-lg text-gray-600">{{ $comment['creator'] }}</p>
                     <p class="mx-3 py-1 text-xs text-gray-500 font-semibold">{{ $comment['created_at'] }}</p>
                 </div>
-                <p class="text-gray-800">
-                    {{ $comment['body'] }}
-                </p>
+                <p class="text-gray-800"> {{ $comment['body'] }} </p>
             </div>
         @endforeach
     </div>
