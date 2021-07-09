10e8fd9da5914cf551d554fddf7cf1d93d36ee87
akasifislam <aseasifislam@gmail.com>
2021-07-09 16:18:55 +0600

    laravel liveware properties and action

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,13 +1,13 @@
 <div class="flex justify-center">
-        <div class="w-6/12">
+        <div class="w-8/12">
             <h1 class="my-10 text-3xl">comments</h1>
-            <div class="my-4 flex">
-            <input type="text" wire:model="newComment" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="what's in your mind">
+            <form class="my-4 flex" wire:submit.prevent="addComment">
+                <input type="text" wire:model.lazy="newComment" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="what's in your mind">
 
-            <div class="py-2">
-                <button wire:click="addComment" class="p-2 bg-blue-500 w-20 rounded shadow text-white">add</button>
-            </div>
-        </div>
+                <div class="py-2">
+                    <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">share</button>
+                </div>
+            </form>
         @foreach ($comments as $comment)
             <div class="rounded border shadow p-3 my-2">
                 <div class="flex justify-start my-2">
